99334ea9fbfbaa108b80c4b635ee83efcfbe0a9f
Caleb <aquaductape@gmail.com>
2019-11-12 10:54:32 -0800

    more types

--- client/src/models/index.ts
+++ client/src/models/index.ts
@@ -2,16 +2,30 @@ export type WakaData = [string, string, number];
 
 export type WakaSchema = [
   {
-    name: 'Project';
-    type: 'string';
+    name: "Project";
+    type: "string";
   },
   {
-    name: 'Time';
-    type: 'date';
-    format: '%-m/%-d/%Y';
+    name: "Time";
+    type: "date";
+    format: "%-m/%-d/%Y";
   },
   {
-    name: 'Total';
-    type: 'number';
+    name: "Total";
+    type: "number";
   }
 ];
+
+export type theme = {
+  primaryColor: string;
+  textColor: string;
+  primaryBg: string;
+};
+
+export interface ISettings {
+  darkMode: boolean;
+  hideHeader: boolean;
+  headerTop: boolean;
+  darkTheme: theme;
+  lightTheme: theme;
+}

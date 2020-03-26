33f27e541e3dbcf1a4f8e54f3284b32817e293b2
Caleb <aquaductape@gmail.com>
2020-03-26 03:30:58 -0700

    changed global listeners back to document, now with touchstart

--- client/src/utils/addEscapeHatch.ts
+++ client/src/utils/addEscapeHatch.ts
@@ -1,25 +1,27 @@
 export default function addEscapeHatch(callback: Function) {
-  const html = document.querySelector("html")!;
-
   const removeOnClick = () => {
     callback();
-    html.removeEventListener("click", removeOnClick);
-    html.removeEventListener("keydown", removeOnEscapeKey);
+    document.removeEventListener("click", removeOnClick);
+    document.removeEventListener("touchstart", removeOnClick);
+    document.removeEventListener("keydown", removeOnEscapeKey);
   };
   const removeOnEscapeKey = (e: KeyboardEvent) => {
     if (!e.key.match(/escape/i)) return null;
     callback();
-    html.removeEventListener("click", removeOnClick);
-    html.removeEventListener("keydown", removeOnEscapeKey);
+    document.removeEventListener("click", removeOnClick);
+    document.removeEventListener("touchstart", removeOnClick);
+    document.removeEventListener("keydown", removeOnEscapeKey);
   };
-  html.addEventListener("click", removeOnClick);
-  html.addEventListener("keydown", removeOnEscapeKey);
+  document.addEventListener("click", removeOnClick);
+  document.addEventListener("touchstart", removeOnClick);
+  document.addEventListener("keydown", removeOnEscapeKey);
 
   return {
     remove() {
       callback();
-      html.removeEventListener("click", removeOnClick);
-      html.removeEventListener("keydown", removeOnEscapeKey);
+      document.removeEventListener("click", removeOnClick);
+      document.removeEventListener("touchstart", removeOnClick);
+      document.removeEventListener("keydown", removeOnEscapeKey);
     }
   };
 }
